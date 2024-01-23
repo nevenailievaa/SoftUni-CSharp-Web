8b03702ee491f055b3b3f369c04710d5f7d318c0
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2024-01-23 12:14:58 +0200

    Add files via upload

---
+++ 01. ASP.NET Core Introduction/03. Text Splitter App/TextSplitterApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitterApp.Models;
+
+namespace TextSplitterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public IActionResult Index(TextViewModel model) => View(model);
+
+        public IActionResult Privacy() => View();
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            var splitToArray = model.Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplitText =String.Join(Environment.NewLine, splitToArray);
+
+            return RedirectToAction("Index", model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
