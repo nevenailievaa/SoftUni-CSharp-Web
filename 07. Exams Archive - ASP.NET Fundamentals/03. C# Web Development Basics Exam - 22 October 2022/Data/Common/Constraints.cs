86706eec591343b171cf98120443875249337e68
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2024-02-12 10:34:00 +0200

    Add files via upload

---
+++ 07. Exams Archive - ASP.NET Fundamentals/03. C# Web Development Basics Exam - 22 October 2022/Data/Common/Constraints.cs
@@ -0,0 +1,30 @@
+﻿namespace Library.Data.Common;
+
+public static class Constraints
+{
+    public static class BookConstraints
+    {
+        //Title
+        public const int BookTitleMinLength = 10;
+        public const int BookTitleMaxLength = 50;
+
+        //Author
+        public const int BookAuthorMinLength = 5;
+        public const int BookAuthorMaxLength = 50;
+
+        //Description
+        public const int BookDescriptionMinLength = 5;
+        public const int BookDescriptionMaxLength = 5000;
+
+        //Rating
+        public const double BookRatingMinValue = 0.00;
+        public const double BookRatingMaxValue = 10.00;
+    }
+
+    public static class CategoryConstraints
+    {
+        //Name
+        public const int CategoryNameMinLength = 5;
+        public const int CategoryNameMaxLength = 50;
+    }
+}
---
+++ 07. Exams Archive - ASP.NET Fundamentals/03. C# Web Development Basics Exam - 22 October 2022/Data/Models/Book.cs
@@ -0,0 +1,50 @@
+﻿namespace Library.Data.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using static Library.Data.Common.Constraints.BookConstraints;
+
+    [Comment("Books for the Library")]
+    public class Book
+    {
+        [Comment("The Primary Key of the Book")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("The Title of the Book")]
+        [Required]
+        [MaxLength(BookTitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Comment("The Author of the Book")]
+        [Required]
+        [MaxLength(BookAuthorMaxLength)]
+        public string Author { get; set; } = null!;
+
+        [Comment("The Description of the Book")]
+        [Required]
+        [MaxLength(BookDescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Comment("The Cover of the Book")]
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Comment("The Rating of the Book")]
+        [Required]
+        [Range(BookRatingMinValue, BookRatingMaxValue)]
+        public decimal Rating { get; set; }
+
+        [Comment("The Book's Category Id")]
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Comment("The Book's Category")]
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+
+        [Comment("The Book's Users")]
+        public ICollection<IdentityUserBook> UsersBooks { get; set; } = new HashSet<IdentityUserBook>();
+    }
+}
